6087806adce865959a1a027bf00168e04a88e3c7
Alexander Golden <4214415+SuperGoldenZ@users.noreply.github.com>
2024-08-28 20:49:23 +0900

    renamed testing for time

---
+++ tests/test_get_time.py
@@ -0,0 +1,57 @@
+import pytest
+import cv2
+import vf_analytics
+
+param_sets = [
+    ["assets/test_images/480p/time/45_00_02.png", "45", "00", 480],
+    ["assets/test_images/480p/time/45_00.png", "45", "00", 480],
+    ["assets/test_images/480p/time/no_time_01.png", "", "00", 480],
+    ["assets/test_images/480p/time/18_00_01.png", "18", "00", 480],
+#    ["assets/test_images/480p/time/42_75.png", "42", "76"],
+#    ["assets/test_images/480p/time/43_45.png", "43", "46"],
+#    ["assets/test_images/480p/time/16_75.png", "16", "76"],
+#    ["assets/test_images/480p/time/20_16.png", "20", "16"],
+#    ["assets/test_images/480p/time/29_66.png", "29", "66"],
+#    ["assets/test_images/480p/time/24_76.png", "24", "76"],
+#    ["assets/test_images/480p/time/30_56.png", "30", "68"],
+#    ["assets/test_images/480p/time/41_26.png", "41", "26"],
+#    ["assets/test_images/480p/time/44_06.png", "44", "06"],
+#    ["assets/test_images/480p/time/44_18.png", "44", "16"],
+#    ["assets/test_images/480p/time/40_16.png", "40", "16"],
+#    ["assets/test_images/480p/time/40_66.png", "40", "66"],
+#    ["assets/test_images/480p/time/40_76.png", "40", "76"],
+#    ["assets/test_images/480p/time/39_46.png", "39", "46"],
+#    ["assets/test_images/480p/time/39_88.png", "39", "66"],
+#    ["assets/test_images/480p/time/37_78.png", "37", "76"],
+#    ["assets/test_images/480p/time/36_88.png", "36", "66"],
+#    ["assets/test_images/480p/time/22_76.png", "22", "76"],
+#    ["assets/test_images/480p/time/30_96.png", "30", "96"],
+#    ["assets/test_images/480p/time/31_16.png", "31", "16"],
+#    ["assets/test_images/480p/time/32_36.png", "32", "36"],
+#    ["assets/test_images/480p/time/33_36.png", "33", "36"],
+#    ["assets/test_images/480p/time/33_18.png", "33", "18"],
+#    ["assets/test_images/480p/time/34_96.png", "34", "28"],
+#    ["assets/test_images/480p/time/35_28.png", "35", "28"],
+#    ["assets/test_images/480p/time/37_68.png", "37", "68"],
+#    ["assets/test_images/480p/time/38_26.png", "38", "26"],
+#    ["assets/test_images/480p/time/41_78.png", "41", "78"],
+#    ["assets/test_images/480p/time/43_96.png", "43", "96"]
+]
+
+@pytest.mark.parametrize("image_filename, expected_time_seconds, expected_time_ms, resolution", param_sets)
+def test_time_480p(image_filename, expected_time_seconds, expected_time_ms, resolution):
+    """Tests OCR for getting time remaining during a match with 480p resolution"""
+
+    vf_analytics.resolution=f"{resolution}p"
+    image = cv2.imread(image_filename)
+
+    assert image is not None, f"{image_filename} is none"
+
+    height = image.shape[0]  # Get the dimensions of the frame
+    assert height == resolution, f"{image_filename} is {height}p instead of expected {resolution}p"
+
+    actual_time_seconds = vf_analytics.get_time_seconds(image)
+    assert expected_time_seconds == actual_time_seconds, f"{actual_time_seconds} not expected value of {expected_time_seconds} for {image_filename}"
+
+    #actual_time_ms = vf_analytics.get_time_ms(image)
+    #assert expected_time_ms == actual_time_ms, f"{actual_time_ms} not expected value of {expected_time_ms}"
