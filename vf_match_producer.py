0230c70cc8efca2d26a9ea37d1c6940986e32fed
Alexander Golden <4214415+SuperGoldenZ@users.noreply.github.com>
2024-08-31 14:08:07 +0900

    will now produce from a file

--- vf_match_producer.py
+++ vf_match_producer.py
@@ -1,24 +1,46 @@
 import os
-import confluent_kafka
+import argparse
 from confluent_kafka import Producer
-import vf_match_analyzer
+import youtube_helper
 
 # Kafka configuration
 conf = {
     "bootstrap.servers": os.environ["KAFKA_BOOTSTRAP_SERVER"],
     "group.id": "youtube_processor_group",
     "auto.offset.reset": "earliest",
-    'security.protocol': 'SASL_SSL',
-    'sasl.mechanism': 'PLAIN',
-    'sasl.username': os.environ["KAFKA_SASL_USERNAME"],
-    'sasl.password': os.environ["KAFKA_SASL_PASSWORD"]
+    "security.protocol": "SASL_SSL",
+    "sasl.mechanism": "PLAIN",
+    "sasl.username": os.environ["KAFKA_SASL_USERNAME"],
+    "sasl.password": os.environ["KAFKA_SASL_PASSWORD"],
 }
+
+
+def produce_from_youtube_playlist(playlist_url):
+    urls = youtube_helper.get_video_urls_from_playlist(playlist_url)
+    for url in urls:
+        print(f"{url}")
+        # producer.produce(topic="youtube_urls", value=url)
+
 
 # Kafka topic to subscribe to
 TOPIC = "youtube_urls"
 
+parser = argparse.ArgumentParser(
+    description="Send list of videos to process to Kafka for queueing"
+)
+parser.add_argument(
+    "--video_file", help="Local text file containing list of YouTube URLs to process"
+)
+
+args = parser.parse_args()
+
 # Create the Kafka consumer
 producer = Producer(conf)
-producer.produce(topic='youtube_urls', value='https://www.youtube.com/watch?v=qdZceuGlaB4')
-producer.flush()
 
+with open(vars(args)["video_file"], "r") as fd:
+    for line in fd:
+        if "watch?v=" in line:
+            producer.produce(topic="youtube_urls", value=line.strip())
+            producer.flush()
+        elif "playlist" in line:
+            produce_from_youtube_playlist(line.strip())
