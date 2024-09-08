c7a896b00217bcb48836e3924822e8a6f29b3d0d
SuperGoldenZ <4214415+SuperGoldenZ@users.noreply.github.com>
2024-09-08 22:36:54 +0900

    Improvement/video flag (#6)
    * adding another test and debug capability
    
    * adding excellent test case
    
    * adding test images
    
    * adding test images
    
    * adding additional tests
    
    * adding test cases
    
    * improved error message and added test functionality
    
    * adding test images
    
    * adding test images
    
    * adding test cases and debugging capability
    
    * bugfixes
    
    * bugfixes
    
    * bugfixes
    
    * adding test images
    
    * adding test data
    
    * added another test
    
    * bugfixes and refactoring
    
    * bugfixes and code formatting
    
    * bugfixes
    
    * test is winning frame
    
    * renamed
    
    * adding additional test cases
    
    * adding additional test cases
    
    * fixing ms bug
    
    * updated
    
    * various bugfixes
    
    * various bugfixes
    
    * various bugfixes
    
    * test images
    
    * debug to false
    
    * added test case
    
    * adding test cases
    
    * updating test cases
    
    * adding additional test cases
    
    * adding test images
    
    * no longer save resized images
    
    * adding test images
    
    * adding test images
    
    * adding test images
    
    * bugfix
    
    * bugfix
    
    * new test cases
    
    * adding flag to force delete videos

--- tests/test_excellent.py
+++ tests/test_excellent.py
@@ -5,6 +5,8 @@ import vf_cv
 
 
 test_data = [
+    ["assets/test_images/1080p/excellent/excellent_for_player_1_04.png", True],
+    ["assets/test_images/1080p/excellent/excellent_for_player_1_03.png", True],
     ["assets/test_images/1080p/excellent/excellent_for_player_1_02.png", True],
     ["assets/test_images/1080p/excellent/excellent_for_player_1_01.png", True],
     ["assets/test_images/480p/excellent/excellent_2_0_01.png", True],
