21784e68271414c1036bcdee8205ddc4d9cbb74d
Alexander Golden <4214415+SuperGoldenZ@users.noreply.github.com>
2024-12-30 10:42:23 +0900

    Formatting

--- vf_cv/revo.py
+++ vf_cv/revo.py
@@ -1,48 +1,35 @@
-import pygetwindow as gw
-import cv2
+"""Detecting Open Beta Splash Screen"""
+
 import numpy as np
-import mss
+import cv2
+
 
 class REVO:
-    REVO_WINDOW_TITLE = "Virtua Fighter5 R.E.V.O."
+    """Detects The Open Beta Splash Screen"""
 
     @staticmethod
-    def get_window_bbox(window_title):
-        """
-        Get the bounding box (left, top, width, height) of the target window.
-        """
-        try:
-            window = gw.getWindowsWithTitle(window_title)[0]
-            if window.isMinimized:  # Ensure the window is not minimized
-                window.restore()
-            bbox = (window.left, window.top, window.right, window.bottom)
-            return bbox
-        except IndexError:
-            print(f"Window with title '{window_title}' not found.")
-            return None
-        
-    def capture_window(window_title=REVO_WINDOW_TITLE):
-        """
-        Capture video stream from a window with a specific title.
-        """
-        bbox = REVO.get_window_bbox(window_title)
-        if bbox is None:
-            return
-        
-        print(f"Capturing window: {window_title} at {bbox}")
-        
-        with mss.mss() as sct:
-            monitor = {"top": bbox[1], "left": bbox[0], 
-                    "width": bbox[2] - bbox[0], "height": bbox[3] - bbox[1]}
-            
-            while True:
-                # Capture the screen region
-                frame = np.array(sct.grab(monitor))
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)  # Remove alpha channel
-                
-                # Show the frame in OpenCV window
-                cv2.imshow("Captured Window", frame)
-                
-                # Exit condition: Press 'q' to exit
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+    def is_beta_screen(image, debug):
+        """Returns true if image is the Open Beta Splash Screen"""
+        roi = image.copy()
+        frame_height = roi.shape[0]
+        gray_image = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+        # Apply a threshold to keep only the bright white colors
+        threshold_value = 200
+
+        _, thresholded_image = cv2.threshold(
+            gray_image, threshold_value, 255, cv2.THRESH_BINARY
+        )
+
+        n_white_pix = 0
+
+        n_white_pix = np.sum(thresholded_image == 255)
+
+        if debug:
+            cv2.imshow(f"threshold {n_white_pix}", thresholded_image)
+
+            cv2.waitKey()
+        if frame_height == 1080 and n_white_pix > 1000000:
+            return True
+
+        return False
