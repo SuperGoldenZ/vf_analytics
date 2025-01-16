802468e676364e166acb0ab4220d129136e0efd1
Alexander Golden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-01-16 20:50:05 +0900

    test wth webp

--- tests/test_knockout.py
+++ tests/test_knockout.py
@@ -1,27 +1,29 @@
+"""Tests for telling if a frame is knocking out another player"""
+
 import pytest
 import cv2
 import vf_cv
 
-test_data = [    
+test_data = [
     "assets/test_images/1080p/ko/knockout_for_player_1_06.png",
-    "assets/test_images/360p/knockout/knockout_43.png",
-    "assets/test_images/360p/knockout/knockout_38.png",
-    "assets/test_images/360p/knockout/knockout_37.png",
-    "assets/test_images/360p/knockout/knockout_35.png",
-    "assets/test_images/360p/knockout/knockout_34.png",
-    "assets/test_images/360p/knockout/knockout_31.png",
-    "assets/test_images/360p/knockout/knockout_14.png",
-    "assets/test_images/360p/knockout/knockout_12.png",
-    "assets/test_images/360p/knockout/knockout_11.png",
-    "assets/test_images/360p/knockout/knockout_10.png",
-    "assets/test_images/360p/knockout/knockout_09.png",
-    "assets/test_images/360p/knockout/knockout_08.png",
-    "assets/test_images/360p/knockout/knockout_07.png",
-    "assets/test_images/360p/knockout/knockout_06.png",
-    "assets/test_images/360p/knockout/knockout_05.png",
-    "assets/test_images/360p/knockout/knockout_04.png",
-    "assets/test_images/360p/knockout/knockout_03.png",
-    "assets/test_images/360p/knockout/knockout_02.png",
+    "assets/test_images/360p/knockout/webp/knockout_43.webp",
+    "assets/test_images/360p/knockout/webp/knockout_38.webp",
+    "assets/test_images/360p/knockout/webp/knockout_37.webp",
+    "assets/test_images/360p/knockout/webp/knockout_35.webp",
+    "assets/test_images/360p/knockout/webp/knockout_34.webp",
+    "assets/test_images/360p/knockout/webp/knockout_31.webp",
+    "assets/test_images/360p/knockout/webp/knockout_14.webp",
+    "assets/test_images/360p/knockout/webp/knockout_12.webp",
+    "assets/test_images/360p/knockout/webp/knockout_11.webp",
+    "assets/test_images/360p/knockout/webp/knockout_10.webp",
+    "assets/test_images/360p/knockout/webp/knockout_09.webp",
+    "assets/test_images/360p/knockout/webp/knockout_08.webp",
+    "assets/test_images/360p/knockout/webp/knockout_07.webp",
+    "assets/test_images/360p/knockout/webp/knockout_06.webp",
+    "assets/test_images/360p/knockout/webp/knockout_05.webp",
+    "assets/test_images/360p/knockout/webp/knockout_04.webp",
+    "assets/test_images/360p/knockout/webp/knockout_03.webp",
+    "assets/test_images/360p/knockout/webp/knockout_02.webp",
     "assets/test_images/720p/knockout/knockout_player_1_02.png",
     "assets/test_images/480p/knockout/knockout_3_1_02.png",
     "assets/test_images/720p/knockout/knockout_player_1_01.png",
@@ -44,15 +46,16 @@ def test_is_knockout(image_filename):
     assert winning_frame.is_ko(DEBUG) is True, f"not Knockout as expected"
 
 
-no_knockout_test_data = [        
-    "assets/test_images/360p/knockout/not_knockout_03.png",
-    "assets/test_images/360p/knockout/not_knockout_02.png",
-    "assets/test_images/360p/knockout/not_knockout_01.png",
+no_knockout_test_data = [
+    "assets/test_images/360p/knockout/webp/not_knockout_03.webp",
+    "assets/test_images/360p/knockout/webp/not_knockout_02.webp",
+    "assets/test_images/360p/knockout/webp/not_knockout_01.webp",
     "assets/test_images/360p/time/35_05.png",
     "assets/test_images/360p/time/33_01.png",
     "assets/test_images/360p/time/30_03.png",
     "assets/test_images/360p/time/27_04.png",
-    "assets/test_images/720p/knockout/no_knockout_01.png"]
+    "assets/test_images/720p/knockout/no_knockout_01.png",
+]
 
 
 @pytest.mark.parametrize("image_filename", no_knockout_test_data)
