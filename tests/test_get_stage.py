c944bb04069502cc80c0487d848045a11f2fe262
Alexander Golden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-01-16 20:42:10 +0900

    webp

--- tests/test_get_stage.py
+++ tests/test_get_stage.py
@@ -6,28 +6,15 @@ import vf_cv.vs_screen
 
 
 test_data = [
-    ["assets/test_images/720p/stage/ruins_01.png", "Ruins"],
-    ["assets/test_images/480p/main_menu.png", None],
-    ["assets/test_images/480p/stage/deep_mountain.png", "Deep Mountain"],
-    ["assets/test_images/480p/stage/genesis.png", "Genesis"],
-    ["assets/test_images/480p/stage/city.jpg", "City"],
-    ["assets/test_images/480p/stage/terrace.png", "Terrace"],
-    ["assets/test_images/480p/stage/sumo_ring.png", "Sumo Ring"],
-    ["assets/test_images/480p/stage/training_room.png", "Training Room"],
-    ["assets/test_images/480p/stage/island_01.jpg", "Island"],
-    ["assets/test_images/480p/stage/island_02.jpg", "Island"],
-    ["assets/test_images/480p/stage/island_03.jpg", "Island"],
-    ["assets/test_images/480p/stage/genesis.png", "Genesis"],
-    ["assets/test_images/480p/stage/aurora.png", "Aurora"],
-    ["assets/test_images/480p/stage/terrace.png", "Terrace"],
-    ["assets/test_images/480p/stage/shrine.png", "Shrine"],
-    ["assets/test_images/480p/stage/sumo_ring.png", "Sumo Ring"],
-    ["assets/test_images/480p/stage/training_room.png", "Training Room"],
-    ["assets/test_images/480p/stage/waterfalls.png", "Waterfalls"],
-    ["assets/test_images/480p/stage/aurora.png", "Aurora"],
-    ["assets/test_images/480p/stage/shrine.png", "Shrine"],
-    ["assets/test_images/480p/stage/waterfalls.png", "Waterfalls"],
-    ["assets/test_images/480p/stage/city.jpg", "City"],
+    ["assets/test_images/480p/stage/webp/aurora.webp", "Aurora"],
+    ["assets/test_images/480p/stage/webp/deep_mountain.webp", "Deep Mountain"],
+    ["assets/test_images/480p/stage/webp/genesis.webp", "Genesis"],
+    ["assets/test_images/480p/stage/webp/none_01.webp", None],
+    ["assets/test_images/480p/stage/webp/shrine.webp", "Shrine"],
+    ["assets/test_images/480p/stage/webp/sumo_ring.webp", "Sumo Ring"],
+    ["assets/test_images/480p/stage/webp/terrace.webp", "Terrace"],
+    ["assets/test_images/480p/stage/webp/training_room.webp", "Training Room"],
+    ["assets/test_images/480p/stage/webp/waterfalls.webp", "Waterfalls"],
 ]
 
 
