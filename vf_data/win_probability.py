6c2fa88a3c6e510d308e4e83f14d1e69d2b9246c
Alexander Golden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-01-27 07:53:12 +0900

    improved display

--- vf_data/win_probability.py
+++ vf_data/win_probability.py
@@ -14,10 +14,12 @@ class WinProbability:
     def __init__(self):
         self.model = joblib.load('logistic_regression_round_win_probability_model.pkl')
 
-    def generate_win_prob_chart_with_single_line(self, round_number, stage, frame_data, frame_num=0, save_to_file = True):
+    def generate_win_prob_chart_with_single_line(self, round_number, stage, frame_data, frame_num=0, save_to_file = True, p1character="", p2character="", winner_player_number=None):
+        last_time_digit = 45
         for frame in frame_data:
             (frame.win_prob_player1, frame.win_prob_player2) = self.get_win_probability(p1health=frame.p1info.health, p2health=frame.p2info.health, stage=stage,time_remaining=frame.time_seconds_remaining)
             frame.elapsed_time = 45-int(frame.time_seconds_remaining)
+            last_time_digit = 45-int(frame.time_seconds_remaining)
 
         # Extract time_remaining and win probabilities for Player 1 (used as win probability for the chart)        
         time_remaining = np.array([frame.elapsed_time for frame in frame_data])
@@ -27,35 +29,40 @@ class WinProbability:
         fig, ax = plt.subplots(figsize=(6, 0.75))
         
         # Plot the win probability line
-        ax.plot(time_remaining, win_prob, 'bo-', label="Win Probability", markersize=3)
+        ax.plot(time_remaining, win_prob, 'bo-', label="Win Probability", markersize=4)
         
         # Set the limits for the Y-axis (0 to 1 for win probability)
         ax.set_ylim(0, 1)
         
         # Customize Y-axis labels: top is 100% for Player 1, bottom is 100% for Player 2, and no label in the middle
+        # va='center'
+        # , ha='left'
         ax.set_yticks([0, 1])
-        ax.set_yticklabels(['Player 2', 'Player 1'], fontsize=12)
+        ax.set_yticklabels([f"P2\n{p2character}", f"P1\n{p1character}"], fontsize=12)
         
+        if (last_time_digit < 45):
+            last_time_digit += 1
+
+        # Set labels and title
+        ax.set_title(f'Round {round_number} Win Probability')
+
+        if winner_player_number == 1:
+            print(f"p{winner_player_number} winner at {last_time_digit}")
+            ax.plot([last_time_digit], [1], 'ro', markersize=12, label="Win!")  # Red dot at the top
+            ax.set_title(f'Round {round_number-1} Win Probability')
+        elif winner_player_number == 2:
+            print(f"p{winner_player_number} winner at {last_time_digit}")
+            ax.plot([last_time_digit], [0.1], 'ro', markersize=12, label="Win!")  # Red dot at the bottom
+            ax.set_title(f'Round {round_number-1} Win Probability')
+
         # Set the X-axis to represent time remaining, from 45 to 0
         ax.set_xlim(0, 45)
         ax.set_xticks(np.arange(45, -1, -5))  # From 45 to 0 with a step of -5 seconds
         ax.set_xticklabels(np.arange(45, -1, -5))  # Make sure labels match the ticks
         
-        # Set labels and title
-        #ax.set_xlabel('Elapsed Time (seconds)')
-        #ax.set_ylabel('Win Probability')
-        ax.set_title('Win Probability Over Time')
         
         # Add a dotted horizontal line at the middle (50% win probability)
         ax.axhline(y=0.5, color='gray', linestyle='--', linewidth=1)
-
-        # Optionally: Adding text labels for the current probability at the last frame
-        #ax.text(time_remaining[-1], win_prob[-1] + 0.05, f"{float(win_prob[-1])*100:.2f}%", ha='center')
-
-        # Add a legend
-        #ax.legend(loc='upper left')
-
-        # Save the figure to a file (you can save to a location for OBS to pick up)
         
         #out_filename=f'r{round_number}_{frame_num}_win_probability_chart.png'
         out_filename = "win_probability.png"
