c1d8f18c4515bd40966a3ad2265ce8df7b71ac70
Alexander Golden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-01-27 23:23:56 +0900

    ability to turn off win probability image

--- vf_cv/config.py
+++ vf_cv/config.py
@@ -12,6 +12,7 @@ class Config:
         self.process_streamed_videos = True
         self.video_download_folder = None
         self.save_image_format = None
+        self.save_win_probability_image = False
 
     @staticmethod
     def load_config(file_path):
@@ -38,11 +39,17 @@ class Config:
 
         config.auto_record = config_parser.getboolean("OBS", "auto_record")
 
-        config.process_streamed_videos = config_parser.getboolean("YouTube", "process_streamed_videos")
+        config.process_streamed_videos = config_parser.getboolean(
+            "YouTube", "process_streamed_videos"
+        )
 
         config.video_download_folder = config_parser.get(
             "YouTube", "video_download_folder"
         )
 
         config.save_image_format = config_parser.get("Settings", "save_image_format")
+
+        config.save_win_probability_image = config_parser.get(
+            "Settings", "save_win_probability_image"
+        )
         return config
