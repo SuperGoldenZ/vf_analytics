d8415204be0bcc8756fc1185140a8042052dbc09
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-02-18 06:23:55 +0900

    formatting

--- obs/obs.py
+++ obs/obs.py
@@ -1,17 +1,21 @@
 import obsws_python as obs
 import traceback
+import threading
 
 
 class ObsHelper:
     SECRET = "iiXm59Yq0HOTSvYj"
     SCENE_NAME = "REVO"
 
-    def __init__(self, display_text=False, win_probability=False):
+    def __init__(self, display_text=False, win_probability=True):
         self.cl = obs.ReqClient(
             host="192.168.56.1", port=4455, password=self.SECRET, timeout=3
         )
         self.display_text = display_text
         self.win_probability = win_probability
+        self.visibility = [False, False]
+        self.win_probability_visibility_flag = False
+        self.vs_win_probability_visibility_flag = False
 
     def set_item_visible(
         self, visibility=True, scene_name=SCENE_NAME, item_name="P1Text"
@@ -55,14 +59,27 @@ class ObsHelper:
     def first_strike(self, playernum):
         if not self.display_text:
             return
-        self.set_item_source(f"P{playernum}Text", "First\nStrike!", color="0xFF000000")
-        self.set_item_source(f"P{playernum}Text Background", color="0x7DD1D1D1")
-        self.set_item_visible(
-            scene_name=ObsHelper.SCENE_NAME, item_name=f"P{playernum}Text"
-        )
-        self.set_item_visible(
-            scene_name=ObsHelper.SCENE_NAME, item_name=f"P{playernum}Text Background"
-        )
+
+        def target():
+            self.set_item_source(
+                f"P{playernum}Text", "First\nStrike!", color="0xFF000000"
+            )
+            self.set_item_source(f"P{playernum}Text Background", color="0x7DD1D1D1")
+
+        thread = threading.Thread(target=target)
+        thread.start()
+
+        if not self.visibility[playernum - 1]:
+            self.set_item_visible(
+                scene_name=ObsHelper.SCENE_NAME, item_name=f"P{playernum}Text"
+            )
+            self.set_item_visible(
+                scene_name=ObsHelper.SCENE_NAME,
+                item_name=f"P{playernum}Text Background",
+            )
+            self.visibility[playernum - 1] = True
+
+        thread.join()
 
     def catbas(self, playernum):
         self.set_item_source(f"P{playernum}Text", "CAT\nBAS", color="0xFF0000FF")
@@ -77,6 +94,7 @@ class ObsHelper:
         if not self.display_text:
             return
 
+        # print(f"\t\tP{playernum} {hits} hit\n{damage}%")
         if hits == 1:
             self.set_item_source(
                 f"P{playernum}Text", f"{hits} hit\n{damage}%", color="0xFFFFFFFF"
@@ -96,76 +114,86 @@ class ObsHelper:
                 color="0x7DD1D1D1",
             )
 
-        self.set_item_visible(
-            scene_name=ObsHelper.SCENE_NAME, item_name=f"P{playernum}Text"
-        )
-        self.set_item_visible(
-            scene_name=ObsHelper.SCENE_NAME, item_name=f"P{playernum}Text Background"
-        )
+        if not self.visibility[playernum - 1]:
+            self.set_item_visible(
+                scene_name=ObsHelper.SCENE_NAME, item_name=f"P{playernum}Text"
+            )
+            self.set_item_visible(
+                scene_name=ObsHelper.SCENE_NAME,
+                item_name=f"P{playernum}Text Background",
+            )
+            self.visibility[playernum - 1] = True
 
     def hide_text_overlay(self, playernum):
-        self.set_item_visible(
-            scene_name=ObsHelper.SCENE_NAME,
-            item_name=f"P{playernum}Text",
-            visibility=False,
-        )
-        self.set_item_visible(
-            scene_name=ObsHelper.SCENE_NAME,
-            item_name=f"P{playernum}Text Background",
-            visibility=False,
-        )
+        if self.visibility[playernum - 1]:
+            self.set_item_visible(
+                scene_name=ObsHelper.SCENE_NAME,
+                item_name=f"P{playernum}Text",
+                visibility=False,
+            )
+            self.set_item_visible(
+                scene_name=ObsHelper.SCENE_NAME,
+                item_name=f"P{playernum}Text Background",
+                visibility=False,
+            )
+            self.visibility[playernum - 1] = False
 
     def vs_win_probability(self, p1probability, p2probability):
         self.set_item_source("P1 VS Text", f"{p1probability}% WP")
         self.set_item_source("P2 VS Text", f"{p2probability}% WP")
 
     def vs_win_probability_visibility(self, visibility):
-        try:
-            self.set_item_visible(
-                scene_name=ObsHelper.SCENE_NAME,
-                item_name=f"P1 VS Text",
-                visibility=visibility,
-            )
+        if self.vs_win_probability_visibility_flag != visibility:
+            try:
+                self.set_item_visible(
+                    scene_name=ObsHelper.SCENE_NAME,
+                    item_name=f"P1 VS Text",
+                    visibility=visibility,
+                )
 
-            self.set_item_visible(
-                scene_name=ObsHelper.SCENE_NAME,
-                item_name=f"P2 VS Text",
-                visibility=visibility,
-            )
+                self.set_item_visible(
+                    scene_name=ObsHelper.SCENE_NAME,
+                    item_name=f"P2 VS Text",
+                    visibility=visibility,
+                )
 
-            self.set_item_visible(
-                scene_name=ObsHelper.SCENE_NAME,
-                item_name=f"P1 VS Text Background",
-                visibility=visibility,
-            )
+                self.set_item_visible(
+                    scene_name=ObsHelper.SCENE_NAME,
+                    item_name=f"P1 VS Text Background",
+                    visibility=visibility,
+                )
 
-            self.set_item_visible(
-                scene_name=ObsHelper.SCENE_NAME,
-                item_name=f"P2 VS Text Background",
-                visibility=visibility,
-            )
+                self.set_item_visible(
+                    scene_name=ObsHelper.SCENE_NAME,
+                    item_name=f"P2 VS Text Background",
+                    visibility=visibility,
+                )
 
-        except Exception as e:
-            return
+            except Exception as e:
+                return
+
+            self.vs_win_probability_visibility_flag = visibility
 
     def win_probability_visibility(self, visibility):
-        try:
-            self.set_item_visible(
-                scene_name=ObsHelper.SCENE_NAME,
-                item_name=f"Win Probability",
-                visibility=visibility,
-            )
+        if self.win_probability_visibility_flag != visibility:
+            try:
+                self.set_item_visible(
+                    scene_name=ObsHelper.SCENE_NAME,
+                    item_name=f"Win Probability",
+                    visibility=visibility,
+                )
 
-            self.set_item_visible(
-                scene_name=ObsHelper.SCENE_NAME,
-                item_name=f"iWin Probability",
-                visibility=visibility,
-            )
+                self.set_item_visible(
+                    scene_name=ObsHelper.SCENE_NAME,
+                    item_name=f"Match Win Probability",
+                    visibility=visibility,
+                )
 
-            self.set_item_visible(
-                scene_name=ObsHelper.SCENE_NAME,
-                item_name=f"Match Win Text",
-                visibility=visibility,
-            )
-        except Exception as e:
-            return
+                self.set_item_visible(
+                    scene_name=ObsHelper.SCENE_NAME,
+                    item_name=f"Match Win Text",
+                    visibility=visibility,
+                )
+            except Exception as e:
+                return
+            self.win_probability_visibility_flag = visibility
